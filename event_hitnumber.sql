af21cfe08b42876ac8078c82d3dd2f1e2b6d420b
BryceMacbookPro <bryce.tsai@iprospect.com>
2023-06-28 16:55:42 +0800

    新增查詢event記錄順序

--- event_hitnumber.sql
+++ event_hitnumber.sql
@@ -1,9 +1,8 @@
 SELECT
   ROW_NUMBER() OVER (PARTITION BY user_pseudo_id ORDER BY user_pseudo_id, event_timestamp) AS user_event_number,
   *
-FROM 
-    -- 更改為您的bigquery project
-    `{project_id}.{dataset_id}.events_*`
+FROM    -- 更改為您的bigquery project
+    `merkle-taiwan-training.ga4_sample_data.events_20201130`
 WHERE
-  event_name = "page_view"
+  event_name NOT IN ("user_engagement", "session_start")
 ORDER BY user_pseudo_id,event_timestamp
